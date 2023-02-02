3478ab3292b760a709ca81864e86ef281ceda7ee
ducpv07 <ducpv@twendeesort.com>
2023-02-02 23:49:29 +0700

    buidl: format-code

---
+++ template/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 0,
+    'react-native/no-inline-styles': 0,
+    'react-hooks/exhaustive-deps': 'off',
+  },
+};
